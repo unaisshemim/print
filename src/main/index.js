cc7e5050609438735c527b3cd365156aeb7805f1
unaisshemim <unaisshemim@gmail.com>
2023-10-07 16:55:27 +0530

    h

--- src/main/index.js
+++ src/main/index.js
@@ -2,6 +2,7 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
+import { PosPrinter } from '@3ksy/electron-pos-printer'
 
 
 const Printer = require('node-thermal-printer');
